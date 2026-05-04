3bfbe4ae215d969faba838b8321f09e39c1d40a3
Jesus <gomez.rojes22@cadiz.salesianos.edu>
2026-05-04 17:56:39 +0200

    Commit inicial del proyecto con las configuraciones

---
+++ src/main/java/com/example/demo/EaseAssistantBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EaseAssistantBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EaseAssistantBackendApplication.class, args);
+	}
+
+}
